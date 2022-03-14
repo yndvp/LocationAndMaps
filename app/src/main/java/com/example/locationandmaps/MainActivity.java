829653c98686086d9af29632a999a013d6c3a2d2
yndvp <58580623+yndvp@users.noreply.github.com>
2022-03-14 09:35:38 -0400

    set up fused location provider

--- app/src/main/java/com/example/locationandmaps/MainActivity.java
+++ app/src/main/java/com/example/locationandmaps/MainActivity.java
@@ -1,13 +1,23 @@
 package com.example.locationandmaps;
 
+import android.Manifest;
 import android.app.Dialog;
 import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.os.Build;
 import android.os.Bundle;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
@@ -23,19 +33,44 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks,
+GoogleApiClient.OnConnectionFailedListener, LocationListener {
 
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
+    private GoogleApiClient client;
+    /*
+        Fused location provider is a location API in Google play services
+        that intelligently combines different signals to provide location information
+        that your app needs
+     */
+    private FusedLocationProviderClient fusedLocationProviderClient;
+    private ArrayList<String> permissionsToRequest;
+    private ArrayList<String> permissions = new ArrayList<>();
+    private ArrayList<String> permissionsRejected = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
         setSupportActionBar(binding.toolbar);
+
+        client = new GoogleApiClient.Builder(this)
+                .addApi(LocationServices.API)
+                .addOnConnectionFailedListener(this)
+                .addConnectionCallbacks(this)
+                .build();
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(MainActivity.this);
+
+        // Add permissions we need to request location of the users
+        permissions.add(Manifest.permission.ACCESS_FINE_LOCATION);
+        permissions.add(Manifest.permission.ACCESS_COARSE_LOCATION);
+
+        permissionsToRequest = permissionsToRequest(permissions);
 
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
@@ -48,6 +83,36 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    private ArrayList<String> permissionsToRequest(ArrayList<String> wantedPermissions) {
+        ArrayList<String> result = new ArrayList<>();
+        /*
+            Add this permission to request if a permission is not requested
+        */
+        for(String perm : wantedPermissions) {
+            if(!hasPermission(perm)) {
+                result.add(perm);
+            }
+        }
+        return result;
+    }
+
+    private boolean hasPermission(String perm) {
+        /*
+            Check if the Android version of the device running the app
+            is higher than Android 8.0
+         */
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            /*
+                checkSelfPermission()
+                => Check if the user has already granted your app a
+                particular permission. This method returns either
+                PERMISSION_GRANTED or PERMISSION_DENIED
+             */
+            return checkSelfPermission(perm) == PackageManager.PERMISSION_GRANTED;
+        }
+        return true;
     }
 
     @Override
@@ -105,5 +170,25 @@ public class MainActivity extends AppCompatActivity {
                     Toast.LENGTH_LONG)
                     .show();
         }
+    }
+
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 }
