32d87e239b1f9f775a8de87e310c7ad2afbb9204
yndvp <58580623+yndvp@users.noreply.github.com>
2022-03-16 23:54:48 -0400

    set up location provider

--- app/src/main/java/com/example/locationandmaps/MainActivity.java
+++ app/src/main/java/com/example/locationandmaps/MainActivity.java
@@ -13,7 +13,10 @@ import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.annotation.NonNull;
@@ -22,6 +25,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
 
+import androidx.core.app.ActivityCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -31,6 +35,7 @@ import com.example.locationandmaps.databinding.ActivityMainBinding;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -41,15 +46,15 @@ GoogleApiClient.OnConnectionFailedListener, LocationListener {
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
     private GoogleApiClient client;
-    /*
-        Fused location provider is a location API in Google play services
-        that intelligently combines different signals to provide location information
-        that your app needs
-     */
     private FusedLocationProviderClient fusedLocationProviderClient;
     private ArrayList<String> permissionsToRequest;
     private ArrayList<String> permissions = new ArrayList<>();
     private ArrayList<String> permissionsRejected = new ArrayList<>();
+    private TextView locationTextView;
+    private LocationRequest locationRequest;
+
+    public static final long UPDATE_INTERVAL = 5000;
+    public static final long FASTEST_INTERVAL = 5000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,6 +62,7 @@ GoogleApiClient.OnConnectionFailedListener, LocationListener {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         setSupportActionBar(binding.toolbar);
+        locationTextView = findViewById(R.id.location_text_view);
 
         client = new GoogleApiClient.Builder(this)
                 .addApi(LocationServices.API)
@@ -64,6 +70,11 @@ GoogleApiClient.OnConnectionFailedListener, LocationListener {
                 .addConnectionCallbacks(this)
                 .build();
 
+        /*
+            Fused location provider is a location API in Google play services
+            that intelligently combines different signals to provide location information
+            that your app needs
+        */
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(MainActivity.this);
 
         // Add permissions we need to request location of the users
@@ -115,6 +126,29 @@ GoogleApiClient.OnConnectionFailedListener, LocationListener {
         return true;
     }
 
+    private void checkPlayServices() {
+        int errorCode = GoogleApiAvailability.getInstance()
+                .isGooglePlayServicesAvailable(this);
+
+        if(errorCode != ConnectionResult.SUCCESS) {
+            Dialog errorDialog = GoogleApiAvailability.getInstance()
+                    .getErrorDialog(this, errorCode, errorCode, new DialogInterface.OnCancelListener() {
+                        @Override
+                        public void onCancel(DialogInterface dialogInterface) {
+                            Toast.makeText(MainActivity.this, "No services",
+                                    Toast.LENGTH_LONG)
+                                    .show();
+                            finish();
+                        }
+                    });
+            errorDialog.show();
+        } else {
+            Toast.makeText(MainActivity.this, "All is good",
+                    Toast.LENGTH_LONG)
+                    .show();
+        }
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -144,42 +178,81 @@ GoogleApiClient.OnConnectionFailedListener, LocationListener {
                 || super.onSupportNavigateUp();
     }
 
-    // Called when activity resume is complete
-    // after onResume() has been called
-    @Override
-    protected void onPostResume() {
-        super.onPostResume();
-
-        int errorCode = GoogleApiAvailability.getInstance()
-                .isGooglePlayServicesAvailable(this);
-
-        if(errorCode != ConnectionResult.SUCCESS) {
-            Dialog errorDialog = GoogleApiAvailability.getInstance()
-                    .getErrorDialog(this, errorCode, errorCode, new DialogInterface.OnCancelListener() {
-                        @Override
-                        public void onCancel(DialogInterface dialogInterface) {
-                            Toast.makeText(MainActivity.this, "No services",
-                                    Toast.LENGTH_LONG)
-                                    .show();
-                            finish();
-                        }
-                    });
-            errorDialog.show();
-        } else {
-            Toast.makeText(MainActivity.this, "All is good",
-                    Toast.LENGTH_LONG)
-                    .show();
-        }
-    }
-
     @Override
     public void onLocationChanged(@NonNull Location location) {
 
     }
 
     @Override
-    public void onConnected(@Nullable Bundle bundle) {
+    protected void onStart() {
+        super.onStart();
+        if(client != null) {
+            client.connect();
+        }
+    }
 
+    @Override
+    protected void onStop() {
+        super.onStop();
+        client.disconnect();
+    }
+
+    // Called when activity resume is complete. after onResume() has been called
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+        checkPlayServices();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        if(client != null && client.isConnected()) {
+            LocationServices.getFusedLocationProviderClient(this)
+                    .removeLocationUpdates(new LocationCallback(){});
+            client.disconnect();
+        }
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        if(ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+            && ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        fusedLocationProviderClient.getLastLocation()
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        // Get last known location. But it could be null
+                        if(location != null){
+                            locationTextView.setText(String.format("Lat: %s Lon: %s", location.getLatitude(), location.getLongitude()));
+                        }
+                    }
+                });
+        startLocationUpdates();
+    }
+
+    private void startLocationUpdates() {
+        locationRequest = new LocationRequest();
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setInterval(UPDATE_INTERVAL);
+        locationRequest.setFastestInterval(FASTEST_INTERVAL);
+
+        if(ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED
+            && ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(MainActivity.this,
+                    "You need to enable permission to display location",
+                    Toast.LENGTH_LONG)
+                    .show();
+        }
     }
 
     @Override
